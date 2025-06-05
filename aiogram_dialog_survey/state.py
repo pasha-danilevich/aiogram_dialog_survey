bd6963f934dbf75dc07daf271fec27d5688121d8
pasha_danilevich <danilevitch.pasha@yandex.ru>
2025-06-05 15:14:25 +0300

    get state

--- aiogram_dialog_survey/state.py
+++ aiogram_dialog_survey/state.py
@@ -9,7 +9,7 @@ from aiogram_dialog_survey.interface import Question
 class StateGroupFactory(StatesGroup):
     first_state_name = "start"
     last_state_name = "end"
-
+    
     @classmethod
     def create_state_group(
         cls, group_name: str, state_names: List[str]
@@ -21,12 +21,12 @@ class StateGroupFactory(StatesGroup):
         :param state_names: Список имен состояний.
         :return: Класс, унаследованный от StatesGroup.
         """
-
+        
         attrs = {name: State() for name in state_names}
-
+        
         # Создаем сам класс с помощью type()
         state_group = type(group_name, (StatesGroup,), attrs)
-
+        
         return state_group  # type: ignore
 
 
@@ -36,7 +36,7 @@ class StateManager(StateGroupFactory):
             name.title(),
             [question.name for question in questions],
         )
-
+    
     def get_first_state(self) -> State:
         state_attributes = {
             name: value
@@ -45,3 +45,6 @@ class StateManager(StateGroupFactory):
         }
         first_state_name = next(iter(state_attributes))
         return state_attributes[first_state_name]
+    
+    def get(self, name: str) -> State:
+        return getattr(self.state_group, name)
